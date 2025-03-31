b902e418e1818f7162d1e7e44de43688a5954e61
Wesley Medeiros Ramalho <112018277+Wesley-Medeiros@users.noreply.github.com>
2025-03-31 14:05:07 -0300

    feat: add cart count

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ import { MegaDropdown } from './modules/megaDropdown.js';
 import { Carrossel } from './modules/carrossel.js';
 import { Dropdown } from './modules/dropdown.js';
 import { HeroCarrossel } from './modules/heroCarrossel.js';
+import { CartCount } from './modules/countCart.js';
 
 Search();
 MegaDropdown();
 Carrossel();
 Dropdown();
 HeroCarrossel()
+CartCount()
